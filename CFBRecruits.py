b8ca4a91669b43b0af9e63ae9688018bd85b369c
Jeremy <34672909+yogermee@users.noreply.github.com>
2018-11-14 18:23:49 -0800

    Update CFBRecruits.py

--- CFBRecruits.py
+++ CFBRecruits.py
@@ -10,6 +10,8 @@ class Scraper(object):
 	def __init__(self):
 		pass
 
+	path = "INSERT YOUR WEB DRIVER'S LOCAL FILE PATH HERE"
+
 	def buildPlayerDicts(self, data_fields, team_player_data, index_iterator):
 		player_data = []
 
@@ -24,11 +26,23 @@ class Scraper(object):
 
 		return player_data
 
+	def calcAvgClassRating(self, final_class_data):
+		total_rating = 0
+
+		for item in final_class_data:
+			for key, value in item.iteritems():
+				if key == "Rating":
+					total_rating += float(value)
+
+		average_rating = total_rating / len(final_class_data)
+
+		return round(average_rating, 2)
+
 class Scrape247(Scraper):
 	def __init__(self):
 		super(Scraper, self).__init__()
 
-	def getURL(self, team, year):
+	def getClassURL(self, team, year):
 		url_prefix = "https://247sports.com/college/"
 		url_suffix = "-Football/Commits/"
 		year = "/Season/" + str(year)
@@ -37,7 +51,7 @@ class Scrape247(Scraper):
 		return url
 
 	def getClassData(self, url):
-		browser = webdriver.Chrome("INSERT YOUR WEB DRIVER'S LOCAL FILE PATH HERE")
+		browser = webdriver.Chrome(self.path)
 
 		try:
 			browser.get(url)
@@ -45,8 +59,6 @@ class Scrape247(Scraper):
 		except TimeoutException as e:
 			browser.execute_script("window.stop();")
 			print "Selenum browser driver has timed out. Please try re-running your script."
-
-		browser.implicitly_wait(30)
 
 		team_player_count = int(re.findall(r'\d+', browser.find_element_by_xpath('//*[@id="page-content"]/div[1]/section[2]/section/div/ul').text.splitlines()[0])[0])
 		team_player_data = str(browser.find_element_by_xpath('//*[@id="page-content"]/div[1]/section[2]/section/div/ul').text).splitlines()[5:]
@@ -57,23 +69,13 @@ class Scrape247(Scraper):
 		return (data_fields, team_player_data, index_iterator)
 
 	def buildClassData(self, data):
-
 		return self.buildPlayerDicts(data[0], data[1], data[2])
 
-	def avgClassRating(self, player_data):
-		total_rating = 0
-
-		for item in player_data:
-			for key, value in item.iteritems():
-				if key == "Rating":
-					total_rating += float(value)
-
-		average_rating = total_rating / len(player_data)
-
-		return round(average_rating, 2)
+	def avgClassRating(self, final_class_data):
+		return self.calcAvgClassRating(final_class_data)
 
 	def teamRankings(self, year):
-		browser = webdriver.Chrome("INSERT YOUR WEB DRIVER'S LOCAL FILE PATH HERE")
+		browser = webdriver.Chrome(self.path)
 
 		url_prefix = "https://247sports.com/Season/"
 		url_suffix = "-Football/CompositeTeamRankings/"
@@ -81,19 +83,18 @@ class Scrape247(Scraper):
 		url = url_prefix + year + url_suffix
 
 		browser.get(url)
-		browser.implicitly_wait(30)
 
-		all_team_data = str(browser.find_element_by_xpath('//*[@id="page-content"]/div/section/section/div/ul').text).splitlines()
+		all_team_data = str(browser.find_element_by_xpath('//*[@id="page-content"]/div/section/section/div/ul').text).splitlines()[8:-1]
 		data_fields = ["Current Rank", "Previous Rank", "Team", "Total Commits",
 		"Average Rating", "5-Stars","4-Stars","3-Stars", "Total Points"]
 
-		team_data_list = all_team_data[8:-1]
-		index_iterator = len(team_data_list) / 50
+		index_iterator = len(all_team_data) / 50
+
 		team_data = []
 
-		for number in range(0, len(team_data_list), index_iterator):
+		for number in range(0, len(all_team_data), index_iterator):
 			team_dict = {}
-			team = team_data_list[number:number + index_iterator]
+			team = all_team_data[number:number + index_iterator]
 
 			for data_item, team_item in zip(data_fields, team):
 				team_dict[data_item] = team_item
@@ -106,15 +107,14 @@ class ScrapeRivals(Scraper):
 	def __init__(self):
 		super(Scraper, self).__init__()
 
-	def getURL(self, team, year):
+	def getClassURL(self, team, year):
 		url = "https://" + team + ".rivals.com/commitments/football/" + str(year)
 
 		return url
 
 	def getClassData(self, url):
-		browser = webdriver.Chrome("INSERT YOUR WEB DRIVER'S LOCAL FILE PATH HERE")
+		browser = webdriver.Chrome(self.path)
 		browser.get(url)
-		browser.implicitly_wait(30)
 
 		data_fields = ["Name", "Position", "Location", "Height", "Weight", "Rating", "Commit Date", "Status"]
 		team_player_data = browser.find_element_by_xpath('//*[@id="articles"]/rv-commitments').text.splitlines()[1:]
@@ -125,5 +125,35 @@ class ScrapeRivals(Scraper):
 		return (data_fields, team_player_data, index_iterator)
 
 	def buildClassData(self, data):
-
 		return self.buildPlayerDicts(data[0], data[1], data[2])
+
+	def avgClassRating(self, final_class_data):
+		return self.calcAvgClassRating(final_class_data)
+
+	def teamRankings(self, year):
+		browser = webdriver.Chrome(self.path)
+
+		url_prefix = "https://n.rivals.com/team_rankings/"
+		url_suffix = "/all-teams/football"
+		year = str(2012)
+		url = url_prefix + year + url_suffix
+
+		browser.get(url)
+
+		all_team_data = str(browser.find_element_by_xpath('//*[@id="articles"]/rv-team-rankings/rv-sortable-table/div/div[2]').text).splitlines()[1:]
+		data_fields = ["Current Rank", "Team", "Total Commits", "5-Stars","4-Stars","3-Stars",
+		"Average Rating", "Total Points"]
+
+		index_iterator = len(all_team_data) / 100
+		team_data = []
+
+		for number in range(0, len(all_team_data), index_iterator):
+			team_dict = {}
+			team = all_team_data[number:number + index_iterator]
+
+			for data_item, team_item in zip(data_fields, team):
+				team_dict[data_item] = team_item
+
+			team_data.append(team_dict)
+
+		return team_data
